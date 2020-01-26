a6f508bd3e62e91522f957317f69b7775ab4d025
Tiago Alves <alvesjtiago@gmail.com>
2020-01-26 15:05:20 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+const { app, Menu, Tray } = require('electron')
+const exec = require('child_process').exec
+const path = require('path')
+var fs = require('fs')
+
+var appIcon = null
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function() {
+  // Menu bar icon
+  appIcon = new Tray(path.join(__dirname, '../assets/images/icon.png'))
+
+  fs.readFile(
+    path.join(
+      app.getPath('home'),
+      `/Library/Application\ Support/Google/Chrome/Local\ State`
+    ),
+    'utf8',
+    function(err, data) {
+      if (err) console.log(err)
+      let menuItems = []
+
+      if (data) {
+        const localState = JSON.parse(data)
+        const profiles = Object.keys(localState.profile.info_cache)
+
+        menuItems = profiles.map(profileName => {
+          displayName = localState.profile.info_cache[profileName].name
+          return {
+            label: displayName,
+            type: 'normal',
+            click: () => {
+              exec(
+                `open -na "Google Chrome" --args --profile-directory="${profileName}"`
+              )
+            }
+          }
+        })
+      }
+
+      menuItems.push({
+        label: 'Quit',
+        type: 'normal',
+        click: () => {
+          app.quit()
+        }
+      })
+
+      const contextMenu = Menu.buildFromTemplate(menuItems)
+      appIcon.setToolTip('Proswitch')
+      appIcon.setContextMenu(contextMenu)
+    }
+  )
+
+  // Hide dock menu
+  app.dock.hide()
+})
